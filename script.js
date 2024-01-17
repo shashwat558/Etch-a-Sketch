68be85d48e20000a4e2449724545ca9731ee8c8a
shashwat558 <tumbaktuhere@gmail.com>
2024-01-17 23:14:33 +0530

    created canvas of 16x16

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 const clrbtn = document.getElementById('clearbtn');
 const erasebtn = document.getElementById('erasebtn');
-const 
+const grid = document.getElementById('grid');
+
+for (let i = 0; i < 16; i++) {
+    let column = document.createElement('div');
+    column.style.display = "flex";
+    column.style.flexDirection = "column";
+    column.style.flexGrow = 1; // Set flex-grow to 1 for the flex container
+
+    for (let j = 0; j < 16; j++) {
+        let rowBlocks = document.createElement('div');
+        rowBlocks.style.backgroundColor = 'white';
+        rowBlocks.style.flex = 1;
+        rowBlocks.style.border = "0.1px solid grey";
+        column.appendChild(rowBlocks);
+    }
+    grid.appendChild(column);
+}
