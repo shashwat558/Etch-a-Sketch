1391a6b052127ed8ff5fcc2af5153acb0ca7c2eb
shashwat558 <tumbaktuhere@gmail.com>
2024-01-18 00:18:58 +0530

    Added a prompt that asks for no. of rows

--- script.js
+++ script.js
@@ -1,19 +1,46 @@
 const clrbtn = document.getElementById('clearbtn');
 const erasebtn = document.getElementById('erasebtn');
 const grid = document.getElementById('grid');
+const sizeChanger = document.getElementById('sizen')
+function createCanvas(size){
+    for (let i = 0; i < size; i++) {
+        let column = document.createElement('div');
+        column.style.display = "flex";
+        column.style.flexDirection = "column";
+        column.style.flexGrow = 1; // Set flex-grow to 1 for the flex container
 
-for (let i = 0; i < 16; i++) {
-    let column = document.createElement('div');
-    column.style.display = "flex";
-    column.style.flexDirection = "column";
-    column.style.flexGrow = 1; // Set flex-grow to 1 for the flex container
+        for (let j = 0; j < size; j++) {
+            let rowBlocks = document.createElement('div');
+            rowBlocks.style.backgroundColor = 'white';
+            rowBlocks.style.flex = 1;
+            rowBlocks.style.border = "0.1px solid grey";
 
-    for (let j = 0; j < 16; j++) {
-        let rowBlocks = document.createElement('div');
-        rowBlocks.style.backgroundColor = 'white';
-        rowBlocks.style.flex = 1;
-        rowBlocks.style.border = "0.1px solid grey";
-        column.appendChild(rowBlocks);
+            column.appendChild(rowBlocks);
+
+        }
+        grid.appendChild(column);
     }
-    grid.appendChild(column);
 }
+
+
+
+
+
+
+
+function gridSize() {
+    const size = parseInt(prompt("Enter the row you want: "));
+    if(!isNaN(size) && size > 0){
+
+        
+        createCanvas(size);
+    } else {
+        prompt('Enter a Valid number')
+    }
+   
+    
+    
+}
+
+
+
