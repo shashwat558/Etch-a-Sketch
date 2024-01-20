87fcf1c9cc19567ac9d7604e0d8d5e2c95125029
shashwat558 <tumbaktuhere@gmail.com>
2024-01-20 21:25:21 +0530

    Completed

--- script.js
+++ script.js
@@ -1,69 +1,24 @@
 
+let paintColor = 'black';
 const clrbtn = document.getElementById('clearbtn');
-const erasebtn = document.getElementById('erasebtn');
+const erasebtn = document.getElementsByClassName('erasebtn');
 const grid = document.getElementById('grid');
 const sizeChanger = document.getElementById('sizen')
-
-function createCanvas(size){
-    for (let i = 0; i < size; i++) {
-        let column = document.createElement('div');
-        column.classList.add('column');
-        column.style.display = "flex";
-        column.style.flexDirection = "column";
-        column.style.flex = 1; // Set flex-grow to 1 for the flex container
-
-        for (let j = 0; j < size; j++) {
-            let rowBlocks = document.createElement('div');
-            rowBlocks.classList.add('rowBlocks')
-            rowBlocks.style.backgroundColor = 'white';
-            rowBlocks.style.flex = 1;
-            rowBlocks.style.border = "0.01px solid black";
-            rowBlocks.style.borderRadius = '3px';
-
-
-            //adding hover effect means when mouse hovered over the rowblocks their color is changed to black
-            rowBlocks.addEventListener('mouseenter', function(){
-                rowBlocks.style.backgroundColor = 'black';
-            })
-
-            
-
-
-
-            column.appendChild(rowBlocks);
-
-        }
-        grid.appendChild(column);
-    }
-}
-
-createCanvas(16)
-
-
-function gridSize() {
-    let size;
-
-    do{
-     size = parseInt(prompt("Enter the row you want: "));
-    if(!isNaN(size) && size > 0 && size <= 64){
-       grid.innerHTML = '';
-        
-        createCanvas(size);
-    }else {
-            // Prompt again until a valid input is provided
-            alert('Enter a valid positive number between 1 and 64 (inclusive).');
-        }
-} while(isNaN(size) || (size<=0 && size > 65) ){
-   
-}     
-    
-}
 function erase(){
     const gridItems = document.querySelectorAll('.column .rowBlocks');
     gridItems.forEach(rowBlocks =>{
         rowBlocks.style.backgroundColor = 'white';
     })
 
+}
+
+function paint(){
+    const gridItems = document.querySelectorAll('.column .rowBlocks');
+    gridItems.forEach(rowBlocks => {
+        rowBlocks.addEventListener('mouseenter', () => {
+            rowBlocks.style.backgroundColor = paintColor;
+        })
+    })
 }
 
 
@@ -78,4 +33,64 @@ function eraser(){
         })
     })
 }
+
+
+
+
+   
+
+    function createCanvas(size){
+        for (let i = 0; i < size; i++) {
+            let column = document.createElement('div');
+            column.classList.add('column');
+            column.style.display = "flex";
+            column.style.flexDirection = "column";
+            column.style.flex = 1; // Set flex-grow to 1 for the flex container
+
+            for (let j = 0; j < size; j++) {
+                let rowBlocks = document.createElement('div');
+                rowBlocks.classList.add('rowBlocks')
+                rowBlocks.style.backgroundColor = 'white';
+                rowBlocks.style.flex = 1;
+                rowBlocks.style.border = "0.01px solid black";
+                
+
+
+                //adding hover effect means when mouse hovered over the rowblocks their color is changed to black
+                rowBlocks.addEventListener('mouseenter', function(){
+                    rowBlocks.style.backgroundColor = paintColor;
+                })
+
+                
+
+                column.appendChild(rowBlocks);
+
+            }
+            grid.appendChild(column);
+        }
+    }
+
+    createCanvas(16)
+
+
+    function gridSize() {
+        let size;
+
+        do{
+        size = parseInt(prompt("Enter the row you want: "));
+        if(!isNaN(size) && size > 0 && size <= 64){
+        grid.innerHTML = '';
+            
+            createCanvas(size);
+        }else {
+                // Prompt again until a valid input is provided
+                alert('Enter a valid positive number between 1 and 64 (inclusive).');
+            }
+    } while(isNaN(size) || (size<=0 && size > 65) ){
+    
+    }     
+        
+    }
+
+
 
